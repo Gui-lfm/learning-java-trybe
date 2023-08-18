57b47e2afad52a4fc53c19cfedbe631273894480
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-08-17 23:02:07 -0300

    requisito 01

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,9 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +13,21 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    Scanner scanner = new Scanner(System.in);
+    Map<String, String> atividadesPesos = new HashMap<>();
+    String inputQtdAtividades = scanner.nextLine();
+
+    short qtdAtividades = Short.parseShort(inputQtdAtividades);
+
+    for (short i = 1; i <= qtdAtividades; i++) {
+      System.out.println("Digite o nome da atividade " + i + ":");
+      String inputNome = scanner.nextLine();
+      System.out.println("Digite o peso da atividade " + i + ":");
+      String inputPeso = scanner.nextLine();
+      atividadesPesos.put(inputNome, inputPeso);
+    }
   }
+
+
 }
