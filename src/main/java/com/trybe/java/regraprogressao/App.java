dca69c081a932383541b5f2bd8c3708c56162c00
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-08-18 00:45:44 -0300

    requisito 03

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,7 +1,7 @@
 package com.trybe.java.regraprogressao;
 
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -15,7 +15,7 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Digite a quantidade de atividades para cadastrar:");
     Scanner scanner = new Scanner(System.in);
-    Map<String, String> atividadesPesos = new HashMap<>();
+    ArrayList<String> atividadesPesos = new ArrayList<>();
     String inputQtdAtividades = scanner.nextLine();
 
     short qtdAtividades = Short.parseShort(inputQtdAtividades);
@@ -25,9 +25,32 @@ public class App {
       String inputNome = scanner.nextLine();
       System.out.println("Digite o peso da atividade " + i + ":");
       String inputPeso = scanner.nextLine();
+      atividadesPesos.add(inputPeso);
       System.out.println("Digite a nota obtida para " + inputNome + ":");
       String inputNota = scanner.nextLine();
-      atividadesPesos.put(inputNota, inputPeso);
+      atividadesPesos.add(inputNota);
+    }
+    short somatoriaPesos = 0;
+    short somatoriaPesosNotas = 0;
+
+    for (int i = 0; i < atividadesPesos.size(); i += 2) {
+      short peso = Short.parseShort(atividadesPesos.get(i));
+      short nota = Short.parseShort(atividadesPesos.get(i + 1));
+
+      somatoriaPesos += peso;
+      somatoriaPesosNotas += (short) (peso * nota);
+    }
+
+    double resultado = (short) (somatoriaPesosNotas / somatoriaPesos);
+
+    if (resultado < 85) {
+      System.out.println(
+          "Lamentamos informar que, com base na sua pontuação alcançada neste período, "
+              + resultado
+              + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
+    } else {
+      System.out.println("Parabéns! Você alcançou " + resultado
+          + "%! E temos o prazer de informar que você obteve aprovação!");
     }
   }
 
